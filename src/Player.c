2dfb2a5eea795bab19b0cb3b61d4beb4bc6ffd1a
jessicacoliveira <149494182+jessicacoliveira@users.noreply.github.com>
2025-12-16 05:43:51 -0300

    Update Player.c

--- src/Player.c
+++ src/Player.c
@@ -1,5 +1,8 @@
 #include "include/Player.h"
-#include "include/Play.h" //notificar morte/gameover 
+
+#define ANIMA_SPEED 4
+#define SPRITE_W 30
+#define SPRITE_H 30
 
 typedef struct{
 	int score;
@@ -9,6 +12,7 @@ typedef struct{
 	int consumed_artifacts_counter;
 } PlayerStats;
 
+
 struct Player{
 	int hp;
 	int extra_life;
@@ -16,9 +20,12 @@ struct Player{
 	int qtd_items_inventory;
 	PlayerStats stats;
 	
-	SDL_Rect position;
+	SDL_Rect position; //esse pedaço tem que sair daqui depois
 	enum DIRECTION_STATE dir;
 	enum MOV_STATE mov;
+	SDL_Texture *texture;
+	int anima_timer;
+	int anima_frame;
 };
 
 typedef struct {
@@ -36,15 +43,16 @@ static RankInfo get_rank_info (enum RANK rank);
 static void set_new_rank(Player *player, enum RANK new_rank);
 static enum RANK check_player_rank (int player_score);
 static void set_RankInfo();
+static void player_load_textures(SDL_Renderer *ren, Player *player);
+static void player_destroy_textures(Player *player);
 
 static RankInfo escoteiro_info, aventureiro_info, pesquisador_info;
 static int init_flag = 0;
 
-Player *create_player() {
+Player *create_player(SDL_Renderer *ren) {
 	if (!init_flag) set_RankInfo();
-	
+
 	Player *player = malloc(sizeof(Player));
-	
     *player = (Player){
         .hp = escoteiro_info.max_hp,
         .extra_life = escoteiro_info.max_extra_life,
@@ -53,14 +61,31 @@ Player *create_player() {
 		.stats = { 0,0,0,0,0 },
 		.position = { 0,0, TILE_SIZE_GAME, TILE_SIZE_GAME },
         .dir = DOWN,
-        .mov = IDLE
+        .mov = IDLE,
+		.texture = NULL,
+		.anima_timer = 0,
+		.anima_frame = 0
     };
+	player_load_textures(ren, player);
     return player;
 }
 
 void update_player_position (Player *player, Point pos){
 	player->position.x = pos.x * TILE_SIZE_GAME;
 	player->position.y = pos.y * TILE_SIZE_GAME;
+}
+
+static void player_load_textures(SDL_Renderer *ren, Player *player){
+	player->texture = IMG_LoadTexture(ren, "assets/img/player/sprite_player.png");
+    assert(player->texture != NULL);
+}
+
+void player_set_idle(Player *player){
+	player->mov = IDLE;
+}
+
+static void player_destroy_textures(Player *player){
+	SDL_DestroyTexture(player->texture);
 }
 
 void update_player_coord (Player *player, enum Type_terrain terrain, enum DIRECTION_STATE direction, int rows, int cols){
@@ -70,10 +95,26 @@ void update_player_coord (Player *player, enum Type_terrain terrain, enum DIRECT
 	else if (terrain == TERRAIN_MUD) v = 5;
 	else v = 0;
 	
-	if (direction == UP) update_player_y (player, -v);
-	else if (direction == DOWN) update_player_y (player, v);
-	else if (direction == RIGHT) update_player_x (player, v);
-	else if (direction == LEFT) update_player_x (player, -v);
+	if (direction == UP){
+		update_player_y (player, -v);
+		player->dir = UP;
+		player->mov = WALKING;
+	}
+	else if (direction == DOWN){
+		update_player_y (player, v);
+		player->dir = DOWN;
+		player->mov = WALKING;
+	}
+	else if (direction == RIGHT){
+		update_player_x (player, v);
+		player->dir = RIGHT;
+		player->mov = WALKING;
+	}
+	else if (direction == LEFT){
+		update_player_x (player, -v);
+		player->dir = LEFT;
+		player->mov = WALKING;
+	}
 	
 	check_map_limits (player, rows, cols);
 
@@ -163,7 +204,7 @@ void update_collected_potions(Player *player){
 
 void update_collected_treasures(Player *player){
 	player->stats.collected_treasures_counter++;
-	if (player->stats.collected_treasures_counter >= MAX_TREASURES - 1){
+	if (player->stats.collected_treasures_counter == MAX_TREASURES){
 		PlayerMsg msg = { .id_msg = PLAYER_VICTORY };
 		Game_Update_Player (&msg);
 	}
@@ -211,6 +252,7 @@ int get_player_score (Player *player){
 }
 
 void destroy_player(Player *player){
+	player_destroy_textures(player);
 	free(player);
 }
 
@@ -223,6 +265,11 @@ int get_player_max_hp(Player *player){
 	return info.max_hp;
 }
 
+int get_player_tam_invent (Player *player){
+	RankInfo info = get_rank_info (player->rank);
+	return info.max_items_inventory;
+}
+
 char *get_rank_player_name (Player *player){
 	RankInfo info = get_rank_info (player->rank);
 	return info.rank_name;
@@ -230,6 +277,18 @@ char *get_rank_player_name (Player *player){
 
 int get_player_extra_life(Player *player){
 	return player->extra_life;
+}
+
+int get_collected_artifacts(Player *player){
+	return player->stats.collected_artifacts_counter;
+}
+
+int get_collected_potions(Player *player){
+	return player->stats.collected_potions_counter;
+}
+
+int get_consumed_artifacts(Player *player){
+	return player->stats.consumed_artifacts_counter;
 }
 
 static void set_new_rank(Player *player, enum RANK new_rank){
@@ -259,7 +318,7 @@ static enum RANK check_player_rank (int player_score){
 
 static void set_RankInfo(){
 	escoteiro_info = (RankInfo){
-		.rank_name = "Escoteiro",
+		.rank_name = "Um",
 		.min_score = 0,
 		.max_hp = 17,
 		.max_extra_life = 3,
@@ -267,7 +326,7 @@ static void set_RankInfo(){
 	};
 	
 	aventureiro_info = (RankInfo){
-		.rank_name = "Escavador",
+		.rank_name = "Dois",
 		.min_score = 50,
 		.max_hp = 22,
 		.max_extra_life = 4,
@@ -275,7 +334,7 @@ static void set_RankInfo(){
 	};
 	
 	pesquisador_info = (RankInfo){
-		.rank_name = "Perito",
+		.rank_name = "Tres",
 		.min_score = 100,
 		.max_hp = 30,
 		.max_extra_life = 5,
@@ -286,14 +345,40 @@ static void set_RankInfo(){
 }
 
 void player_render(Player *player, SDL_Renderer *ren, int cam_x, int cam_y){
-	SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+    int row = 0;
+    switch(player->dir) {
+        case DOWN:  row = 0; break;
+        case RIGHT: row = 2; break;
+        case LEFT:  row = 1; break;
+        case UP:    row = 3; break;
+        default:    row = 0;
+    }
+
+    int col = 0;
+    if (player->mov == IDLE) {
+        col = 0;
+        player->anima_timer = 0;
+    } else {
+        player->anima_timer++;
+        
+        int walk_cycle[] = { 0, 1, 0, 2 }; 
+        int index = (player->anima_timer / ANIMA_SPEED) % 4;
+        col = walk_cycle[index];
+    }
+
+    SDL_Rect src_rect = {
+        col * SPRITE_W, 
+        row * SPRITE_H, 
+        SPRITE_W, 
+        SPRITE_H
+    };
+
+    SDL_Rect dst_rect = {
+        player->position.x - cam_x,
+        player->position.y - cam_y,
+        player->position.w, 
+        player->position.h 
+    };
 	
-	SDL_Rect player_screen = {
-		player->position.x - cam_x,
-		player->position.y - cam_y,
-		player->position.w,
-		player->position.h
-	};
-	
-	SDL_RenderFillRect(ren, &player_screen);
-}	
+    SDL_RenderCopy(ren, player->texture, &src_rect, &dst_rect);
+}
